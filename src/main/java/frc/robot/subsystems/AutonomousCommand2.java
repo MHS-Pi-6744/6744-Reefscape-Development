1bf96743ef07e6117343716f03516ea466aac936
6744Leo <149526582+6744Leo@users.noreply.github.com>
2025-01-20 12:12:00 -0600

    Auto-Changes

--- src/main/java/frc/robot/subsystems/AutonomousCommand2.java
+++ src/main/java/frc/robot/subsystems/AutonomousCommand2.java
@@ -46,9 +46,9 @@ public class AutonomousCommand2 extends Command {
         // Start at the origin facing the +X direction
         new Pose2d(0, 0, new Rotation2d(0)),
         // Pass through these two interior waypoints, making an 's' curve path
-        List.of(new Translation2d(0.1, .1), new Translation2d(3, -1)),
+        List.of(new Translation2d(0.1, .1), new Translation2d(.3, -.1)),
         // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(2, 0, new Rotation2d(0)),
+        new Pose2d(.5, 0, new Rotation2d(0)),
         config);
 
     var thetaController = new ProfiledPIDController(
