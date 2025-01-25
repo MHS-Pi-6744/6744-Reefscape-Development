dafc61a22343cf266fbb3ef076c6588e6b5c7445
MattheDev53 <matthew.c.albrecht@gmail.com>
2025-01-25 10:27:17 -0600

    Create New Motors

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,7 +1,12 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ElevatorConstants;
 
 public class ElevatorSubsystem extends SubsystemBase {
-    
+    private final SparkMax m_shepherd = new SparkMax(ElevatorConstants.kShepherdCanId, SparkMax.MotorType.kBrushless);
+    private final SparkMax m_sheep = new SparkMax(ElevatorConstants.kSheepCanId, SparkMax.MotorType.kBrushless);
 }
