391c696aa3d7e1c81429ca30ce10ba79b1c635f2
MattheDev53 <matthew.c.albrecht@gmail.com>
2025-01-25 11:54:11 -0600

    Set up SparkMax Motors

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,12 +1,42 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
 public class ElevatorSubsystem extends SubsystemBase {
-    private final SparkMax m_shepherd = new SparkMax(ElevatorConstants.kShepherdCanId, SparkMax.MotorType.kBrushless);
-    private final SparkMax m_sheep = new SparkMax(ElevatorConstants.kSheepCanId, SparkMax.MotorType.kBrushless);
+  private SparkMax m_shepherd;
+  private SparkMax m_sheep;
+
+  public ElevatorSubsystem() {
+    m_shepherd = new SparkMax(ElevatorConstants.kShepherdCanId, SparkMax.MotorType.kBrushless);
+    m_sheep = new SparkMax(ElevatorConstants.kSheepCanId, SparkMax.MotorType.kBrushless);
+
+    SparkMaxConfig shepherdConfig = new SparkMaxConfig();
+    shepherdConfig
+      .inverted(false)
+      .idleMode(IdleMode.kBrake)
+      .smartCurrentLimit(ElevatorConstants.kCurrentLimit);
+    shepherdConfig.encoder
+      .positionConversionFactor(ElevatorConstants.kPositionConversionFactor)
+      .velocityConversionFactor(ElevatorConstants.kVelocityConversionFactor);
+    shepherdConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .pid(ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD);
+    SparkMaxConfig sheepConfig = shepherdConfig;
+    sheepConfig
+      .inverted(true);
+
+    m_shepherd.configure(shepherdConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_sheep.configure(sheepConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+
 }
