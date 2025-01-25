2c1af3ae3399162133445e4e4f1464e7fa483640
MattheDev53 <matthew.c.albrecht@gmail.com>
2025-01-25 11:52:34 -0600

    Add New Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -65,6 +65,16 @@ public final class Constants {
   public static final class ElevatorConstants {
     public static final int kShepherdCanId = -1;
     public static final int kSheepCanId = -1;
+
+    public static final int kStartingPosition = 0;
+    public static final int kCurrentLimit = 0;
+
+    public static final int kPositionConversionFactor = 0;
+    public static final int kVelocityConversionFactor = 0;
+    
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
   }
 
   public static final class ModuleConstants {
